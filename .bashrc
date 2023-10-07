9698b40a692c35634a21af5c81fe3f397dd23eef
Keenan D. Buckley <keenandbuckley@protonmail.com>
2023-10-06 18:07:28 -0600

    Create simple node to detect images from realsense

--- .bashrc
+++ .bashrc
@@ -1,3 +1,5 @@
 source /opt/ros/humble/setup.bash
 
-alias start_realsense="ros2 launch realsense2_camera rs_launch.py pointcloud.enable:=true"
+alias start_realsense="cd ~/mines_ws && ros2 launch realsense2_camera rs_launch.py pointcloud.enable:=true"
+alias ros_build="cd ~/mines_ws && colcon build --packages-select data_collection"
+alias camera_launch="cd ~/mines_ws && source install/setup.bash && ros2 run data_collection camera"
---
+++ src/data_collection/data_collection/realsense_collection.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+
+class RealsenseSubscriber(Node):
+    def __init__(self):
+        super().__init__('realsense_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/color/image_rect_raw',
+            self.listener_callback,
+            10)
+        self.subscription
+
+    def listener_callback(self, image):
+        self.get_logger().info(f'Recieved image of shape: {image.width}x{image.height}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    realsense_subscriber = RealsenseSubscriber()
+
+    rclpy.spin(realsense_subscriber)
+
+    realsense_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
