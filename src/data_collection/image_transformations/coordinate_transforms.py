5595674b14601338873a76a6f92f31e21d65e336
David Munro <dmunro@mines.edu>
2023-11-16 21:36:56 -0700

    Added keypoint visualization

--- src/data_collection/image_transformations/coordinate_transforms.py
+++ src/data_collection/image_transformations/coordinate_transforms.py
@@ -40,7 +40,7 @@ class TransformationMatrix:
         
         return (self.matrix @ pose)[:3]
 
-    def inverse_transform(self, pose=np.zeros(3)):
+    def inverse_transform(self, pose=np.zeros(3)) -> 'TransformationMatrix':
         mat_copy = self.matrix.copy()
 
         self.matrix = np.linalg.inv(self.matrix)
@@ -59,19 +59,32 @@ class TransformationMatrix:
         pose = self.matrix[:3,3]
         return pose, orientation
 
-    @ staticmethod
-    def transform_transform(H1=None, H2=None):
-        assert H1 is not None
-        if isinstance(H1, list):
+    def invert(self):
+        return TransformationMatrix( H = np.linalg.inv(self.matrix) )
 
-            matrix = TransformationMatrix.__transformation_matrix()
-            for mat in H1:
-                assert isinstance(mat, TransformationMatrix)
-                matrix = mat.matrix @ matrix
-        elif isinstance(H1, TransformationMatrix) and isinstance(H2, TransformationMatrix):
-            return H1 @ H2
-        else:
-            raise TypeError("Must input Transformation Matrix or List if transformation matricies")
+
+    def __mul__(self, H) -> 'TransformationMatrix':
+        
+        if isinstance(H, np.ndarray):
+
+            if H.shape == (4,4):
+                return TransformationMatrix(H=self.matrix @ H)
+            
+            elif H.shape == (3,3):
+                H = TransformationMatrix(R=H)
+                return TransformationMatrix(H=self.matrix @ H.matrix)
+            
+            elif H.shape == (3,) or H.shape == (3,1) or H.shape == (1,3):
+                
+                H = TransformationMatrix(t = H)
+                return TransformationMatrix(H=self.matrix @ H.matrix)
+            
+        elif isinstance(H, TransformationMatrix):
+
+            if H.matrix.shape == (4,4):
+                return TransformationMatrix(H=self.matrix @ H.matrix)
+            
+        raise ValueError("Must multiply by a 4x4 numpy array of Transformation Matrix")
 
     @ staticmethod
     def __transformation_matrix(translation : np.ndarray = np.zeros(3), rotation : np.ndarray = np.eye(3)) -> np.ndarray:
@@ -130,8 +143,10 @@ class IntrinsicsMatrix:
 
     def calc_pixels(self, pose=[0,0,1]):
         # print(pose)
-        assert len(pose) == 3
-        assert pose[2] > 0
+        if len(pose) != 3:
+            raise ValueError("Position must be in 3D cartesian Coordinates -> ", pose)
+        if pose[2] <= 0:
+            raise ValueError("Z coordinate must be positive to be seen by the camera, z =", pose[2])
 
         pose = np.array(pose, dtype=np.float32)
         coords = self.matrix @ pose
@@ -150,14 +165,16 @@ class IntrinsicsMatrix:
         Returns:
             np.array: (3x3) position (x,y,z)
         '''
-        assert len(pixels) == 2
-        assert depth > 0
+        if len(pixels) != 2:
+            raise ValueError(f"Must give pixels in 2D cartesian coordinates: ({pixels[0]},{pixels[1]})")
+        if depth <= 0:
+            raise ValueError("Depth must be a positive number to be seen by camera ->", depth)
 
         pixels = depth * np.array(list(pixels) + [1])
         return np.linalg.inv(self.matrix) @ pixels
 
 
-def calculate_matrix(x: float, y: float, z: float, angle_mount: float = 0, angle_cap: float = 0) -> TransformationMatrix:
+def calculate_matrix(x: float, y: float, z: float, angle_mount: float = 0, angle_cap: float = 0, units:str="in") -> TransformationMatrix:
     """
     Calculates the transformation matrix from the camera to the fuel cap using the data collection rig
     Assumes fuel cap coordinates to be positive x in the left direction on wall, positive y down, positive z towards camera
@@ -169,25 +186,59 @@ def calculate_matrix(x: float, y: float, z: float, angle_mount: float = 0, angle
         z (float): distance from the rig to the wall
         angle_mount (float): angle measured at the camera mount
         angle_cap (float): angle of the fuel cap on the wall (use protractor)
-    
+        inits (in): the unit system 
     Returns:
         np.ndarray: translation matrix from camera to fuel cap
     """
-    R_cap = Rotation.from_euler('z',angle_cap, degrees=True).as_matrix()
-    H_cap = TransformationMatrix(R=R_cap)
 
-    t_cap_to_mount = np.array([x,y,z])
-    R_cap_to_mount = Rotation.from_euler('y',angle_mount+180, degrees=True).as_matrix()
-    H_cap_to_mount = TransformationMatrix(R=R_cap_to_mount, t=t_cap_to_mount)
+    if units not in ["in", "mm", "cm"]:
+        raise ValueError("Unit system must be in inches, centimeters, or milimeters ->", units)
+    
+    if units == "in":
+        x,y,z = [i * 25.4 for i in [x,y,z]]
+    if units == "cm":
+        x,y,z = [i * 10 for i in [x,y,z]]
 
-    t_mount_to_cam = __mount_to_camera_translation()
-    H_mount_to_cam = TransformationMatrix(t=t_mount_to_cam)
+    # shrink y by heigh of the mount, which is 1.25 inches
+    y -= 25.4 * (1.25 + 0.25)
+    
+    H = TransformationMatrix()
+    H = H * Rotation.from_euler('z', -angle_cap, degrees=True).as_matrix()
+    H = H * np.array([x,y,z])
+    H = H * Rotation.from_euler('y', angles=180+angle_mount, degrees=True).as_matrix()
+    H = H * __mount_to_camera_translation()
 
-    H_cap_to_cam = H_cap.as_mat() @ H_cap_to_mount.as_mat() @ H_mount_to_cam.as_mat()
-    return TransformationMatrix(H=np.linalg.inv(H_cap_to_cam))
+    return H.invert()    
 
 
-def annotate_img(img: np.ndarray, H: TransformationMatrix, K: IntrinsicsMatrix, line_width=5, axis_len = 5) -> np.ndarray:
+
+    
+    
+    # H = TransformationMatrix()
+    # H = H * t_mount_to_cam
+    # H = H * R_mount_to_cam
+    # H = H * t_wall_to_mount
+    # H = H * R_cap_to_wall
+    # return H
+
+
+   
+    # R_cap = Rotation.from_euler('z',angle_cap, degrees=True).as_matrix()
+    # H_cap = TransformationMatrix(R=R_cap)
+
+    # t_cap_to_mount = np.array([x,y,z])
+
+    # R_cap_to_mount = Rotation.from_euler('y',angle_mount+180, degrees=True).as_matrix()
+    # H_cap_to_mount = TransformationMatrix(R=R_cap_to_mount, t=t_cap_to_mount)
+
+    # t_mount_to_cam = __mount_to_camera_translation()
+    # H_mount_to_cam = TransformationMatrix(t=t_mount_to_cam)
+
+    # H_cap_to_cam = H_cap.as_mat() @ H_cap_to_mount.as_mat() @ H_mount_to_cam.as_mat()
+    # return TransformationMatrix(H=np.linalg.inv(H_cap_to_cam))
+
+
+def annotate_img(img: np.ndarray, H: TransformationMatrix, K: IntrinsicsMatrix, line_width=3, axis_len = 30) -> np.ndarray:
     """
     Plots coordinate axis on image
 
@@ -219,20 +270,25 @@ def annotate_img(img: np.ndarray, H: TransformationMatrix, K: IntrinsicsMatrix, 
     return img
 
 
-def __mount_to_camera_translation(cm=True) -> np.ndarray:
+def __mount_to_camera_translation(units: str="mm") -> np.ndarray:
     """
     camera translation based on datasheet information
     camera origin is located on left camera, inset 3.07 mm
     Default to return translation in centimeters
     """
-    x = -9          # cameras are 18 mm apart
-    y = 42 / 2          # cameras are located on middle of camera in y, and cam is 42 mm tall
-    z = -3.7
+    __check_inits(units)
+    # return np.zeros(3)
+    x = 9          # cameras are 18 mm apart
+    y = -42 / 2          # cameras are located on middle of camera in y, and cam is 42 mm tall
+    z = 3.7
 
     trans = np.array([x,y,z], dtype = np.float32)
-    if cm:
-        return trans / 10
+    
     return trans
+
+def __check_inits(units):
+    if units not in ["mm", "cm", "in"]:
+        raise ValueError("Units must be centimeters, milimeeters or inches ->", units)
 
 
 def main():
@@ -243,6 +299,8 @@ def main():
     target_pos = [-3.33183754, -1.87415862,  3.]
     tol = 1e-5
 
+
+    # tests
     assert K.calc_pixels() == [640, 360]
     assert K.calc_pixels(pos) == [928, 648]
     assert [int(i) for i in list(K.calc_position())] == [0, 0, 1]
@@ -250,12 +308,16 @@ def main():
     assert np.sum(pos - np.array(target_pos)) < tol
 
 
-    img = cv2.imread("data//saved_img.png")
-    
+    img = cv2.imread("data/saved_img.png")
     K = IntrinsicsMatrix()
     translation = calculate_matrix(-6, 19.05, 45.72, angle_mount=-10, angle_cap=20)
-    pos, orien = translation.as_pos_and_quat()
-    img = annotate_img(img, translation, K)
+    # pos, orien = translation.as_pos_and_quat()
+
+    # print(pos, orien)
+
+    rot = Rotation.from_euler("y", np.deg2rad(180)).as_matrix()
+
+    img = annotate_img(img, translation, K, axis_len=30)
 
     cv2.imshow("Annotated Image", img)
     cv2.waitKey(0)
